6f637d40a96cbfd093fc2842836f3a441dd65309
Niz <143238220+MMADUs@users.noreply.github.com>
2023-11-02 22:36:47 +0700

    Update LoginView.java

--- src/main/java/com/example/application/views/login/LoginView.java
+++ src/main/java/com/example/application/views/login/LoginView.java
@@ -1,25 +1,24 @@
 package com.example.application.views.login;
 
-import com.example.application.views.MainLayout;
-import com.vaadin.flow.component.Composite;
-import com.vaadin.flow.component.dependency.Uses;
-import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.login.LoginForm;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
 
-@PageTitle("Login")
-@Route(value = "my-view", layout = MainLayout.class)
-@Uses(Icon.class)
-public class LoginView extends Composite<VerticalLayout> {
+@Route("login")
+@PageTitle("LOGIN")
+@AnonymousAllowed
+public class LoginView extends VerticalLayout {
+    private final LoginForm login = new LoginForm();
 
     public LoginView() {
-        LoginForm loginForm = new LoginForm();
-        getContent().setHeightFull();
-        getContent().setWidthFull();
-        getContent().setAlignSelf(FlexComponent.Alignment.CENTER, loginForm);
-        getContent().add(loginForm);
+        addClassName("LoginView");
+        setSizeFull();
+        setAlignItems(Alignment.CENTER);
+        login.setAction("login");
+
+        add(new H1("Login Page"), login);
     }
 }
